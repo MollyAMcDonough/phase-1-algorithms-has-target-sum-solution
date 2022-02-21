c4043fea00603b2f6150cf76c2797df574034d09
Molly Ann McDonough <mcdonough.mollya@gmail.com>
2022-02-21 18:24:29 -0500

    commit tree 7e62f6dcc0a1d1c0d7b27a8aeaa96b2ccfe2de83

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  /*
+  for (let i = 0; i < array.length; i++) {
+    const complement = target - array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === complement) {
+        return true
+      }
+    }
+  }
+  return false;
+  */
+  const seenNumbers = {};
+  for (number of array) {
+    const complement = target - number;
+   
+    if (seenNumbers[complement]) {
+      return true;
+    } 
+    seenNumbers[number] = true;
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  runtime: O(n^2)
+  space: O(n)
+  approach 2:
+  runtime: O(n)
+  space: O(n)
 */
 
 /* 
-  Add your pseudocode here
+  Add your pseudocode here.
+  Iterate through array, find complement for each number and check rest of
+  array for that complement. Return true if match found.
+
+  approach 2: 
+  create an object to keep track of numbers we've already seen
+  iterate through each number in the array. 
+  For the current number, identify the compliment, check object
+  for that complement.
 */
 
 /*
   Add written explanation of your solution here
+  function that takes in array of integers and target. Returns true if 
+  the sum of any two numbers in the array = target. Otherwise
+  returns false
 */
 
 // You can run `node index.js` to view these console logs
@@ -27,8 +64,38 @@ if (require.main === module) {
 
   console.log("");
 
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([1, 2, 2, 5], 4));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([-1, 2, 2, 5], 4));
+
+  console.log("");
+
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([], 0));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([0, 0, 5], 0));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([0, 0, 5], 5));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([5], 0));
 }
 
 module.exports = hasTargetSum;
